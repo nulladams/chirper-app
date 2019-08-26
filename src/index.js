d248260321df611ea0b7786ca358f387c1eb2b05
Leonardo Adams <leonardoadams@gmail.com>
2019-08-25 21:36:47 -0300

    Fix typos

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import reducer from './reducers'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers'
 
 const store = createStore(reducer)
 
