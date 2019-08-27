10ed11bd12a6c22a39d55a45c977f2ce836044e6
Leonardo Adams <leonardoadams@gmail.com>
2019-08-27 18:54:33 -0300

    Add new tweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
     state={
@@ -15,14 +17,13 @@ class NewTweet extends Component {
         e.preventDefault()
 
         const { text } = this.state
+        const { dispatch, id } = this.props
 
-        // todo: Add Tweet to Store
-
-        console.log('New Tweet', text)
+        dispatch(handleAddTweet(text, id))
 
         this.setState(() => ({
             text: ''
-        }))
+        })) 
     }
     render() {
         const { text } = this.state
@@ -59,4 +60,4 @@ class NewTweet extends Component {
     }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
